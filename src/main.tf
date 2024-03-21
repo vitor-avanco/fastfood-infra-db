9db9f25b0e8f9c24717634138e555fc6c48214da
vitor_avanco <vitor.avanco@hotmail.com>
2024-03-20 22:12:35 -0300

    chore: pipeline

---
+++ src/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  backend "s3" {
+    bucket         = "767397811632-terraform-backend"
+    key            = "github/vitor-avanco/fastfood-infra-db"
+    dynamodb_table = "767397811632-terraform-backend"
+    region         = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+  default_tags {
+    tags = {
+      terraform = "true"
+    }
+  }
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
